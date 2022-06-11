874e3071e4ef0a9e943cb9bb3c4ab77d0300fcf9
Miroslav Tashonov <miroslavtashonov@gmail.com>
2022-06-11 19:56:03 +0200

    Add exception handling

--- api/app.py
+++ api/app.py
@@ -16,29 +16,36 @@ def home():
 @app.route('/api/runtests/<filename>')
 def runtests(filename):
     mutex.acquire()
-    task_names = ['Greatest common divisor', 'Message validation', '(De)Serialize to Cell', 'Merge hashmaps', 'Address encoder']
-    #filename = escape(filename)
-    filename_without_extension = str(filename).replace(".tar.gz", "")
-    extract_path = f'../participant-files/{filename_without_extension}'
-
-    exist = os.path.exists(extract_path)
-    if not exist:
-        os.makedirs(extract_path)
-
-    shutil.copytree('../func-contest-files/', f'{extract_path}/func-contest-files')
-    shutil.unpack_archive(f'../participant-files/{filename}', f'{extract_path}/func-contest-files/func')
-
     result_string = ''
-    os.chdir(f'{extract_path}/func-contest-files')
-    for i in range(1, 2):
-        test_result = os.popen(f'toncli run_tests -c contest-{i}').read()
-        index = test_result.find('Test result:')
-        score = test_result[index+20:index+24]
-        result_string += f'TASK {i} - {task_names[i-1]} score is : {score}/100 \n'
-    
-    os.chdir('../../../api')
+    try:
+        task_names = ['Greatest common divisor', 'Message validation', '(De)Serialize to Cell', 'Merge hashmaps', 'Address encoder']
+        #filename = escape(filename)
+        filename_without_extension = str(filename).replace(".tar.gz", "")
+        extract_path = f'../participant-files/{filename_without_extension}'
 
-    shutil.rmtree(extract_path)
+        exist = os.path.exists(extract_path)
+        if not exist:
+            os.makedirs(extract_path)
 
-    mutex.release()
+        shutil.copytree('../func-contest-files/', f'{extract_path}/func-contest-files')
+        shutil.unpack_archive(f'../participant-files/{filename}', f'{extract_path}/func-contest-files/func')
+
+        os.chdir(f'{extract_path}/func-contest-files')
+        for i in range(1, 2):
+            test_result = os.popen(f'toncli run_tests -c contest-{i}').read()
+            index = test_result.find('Test result:')
+            score = test_result[index+20:index+24]
+            result_string += f'TASK {i} - {task_names[i-1]} score is : {score}/100 \n'
+        
+        os.chdir('../../../api')
+
+        shutil.rmtree(extract_path)
+
+        mutex.release()
+    except BaseException as err:
+        mutex.release()
+        if(os.getcwd().find('api') == -1):
+            os.chdir('../../../api')
+        result_string = err
+
     return (result_string)
